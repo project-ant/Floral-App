b72c1247068e78b562ce1c6e4b03da46ce3cfec7
Arter Tendean <arter.tendean.07@gmail.com>
2021-05-04 20:16:56 +0800

    move from expo to react-native-cli

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import "react-native-gesture-handler";
-import { NavigationContainer } from "@react-navigation/native";
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import 'react-native-gesture-handler';
+import {NavigationContainer} from '@react-navigation/native';
 
-import Router from "./src/router";
+import Router from './src/router';
 
 export default function App() {
   return (
