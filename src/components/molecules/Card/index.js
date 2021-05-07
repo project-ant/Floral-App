ad59ff99019266ee67faa8535af8c708a35813b3
Arter Tendean <arter.tendean.07@gmail.com>
2021-05-07 15:02:06 +0800

    update Card

--- src/components/molecules/Card/index.js
+++ src/components/molecules/Card/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import {View, Text, Image} from 'react-native';
+import {View, Text, Image, TouchableOpacity} from 'react-native';
 
 function Card() {
+  const handleCLick = () => {
+    console.log('jalan');
+  };
+
   return (
     <View
       style={{
-        height: 250,
+        height: 257,
         alignItems: 'center',
         borderRadius: 2,
         borderColor: 'black',
         borderWidth: 0.3,
         backgroundColor: 'white',
         margin: 10,
+        paddingBottom: 4,
+        flex: 1,
       }}>
       <Image
         source={require('../../../assets/images/dummy.jpg')}
@@ -23,6 +29,39 @@ function Card() {
       <Text style={{fontFamily: 'Poppins-Medium', fontSize: 15}}>
         Rp. 150.000
       </Text>
+
+      <View
+        style={{
+          width: '100%',
+          padding: 0,
+          flex: 2,
+          flexDirection: 'row',
+          height: 20,
+        }}>
+        <View
+          style={{
+            width: '80%',
+          }}></View>
+        <View
+          style={{
+            width: '20%',
+            backgroundColor: 'white',
+          }}>
+          <TouchableOpacity onPress={handleCLick}>
+            <Text
+              style={{
+                borderColor: 'black',
+                borderWidth: 3,
+                borderRadius: 5,
+                textAlign: 'center',
+                marginRight: 5,
+                fontWeight: '500',
+              }}>
+              +
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 }
