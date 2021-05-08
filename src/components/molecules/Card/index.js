30b896e7cf9682bf06282f3aa2af7af4c0b0c9fd
Arter Tendean <arter.tendean.07@gmail.com>
2021-05-09 00:11:27 +0800

    update HomePage & Card

--- src/components/molecules/Card/index.js
+++ src/components/molecules/Card/index.js
@@ -1,32 +1,23 @@
 import React from 'react';
-import {View, Text, Image, TouchableOpacity} from 'react-native';
+import {View, Text, Image, TouchableOpacity, StyleSheet} from 'react-native';
 
-function Card() {
-  const handleClick = () => {
-    console.log('jalan');
+function Card(props) {
+  const height = props.height ? props.height : 257;
+  const handleClick = e => {
+    console.log(e);
   };
 
   return (
-    <View
-      style={{
-        height: 257,
-        alignItems: 'center',
-        borderRadius: 2,
-        borderColor: 'black',
-        borderWidth: 0.5,
-        backgroundColor: 'white',
-        margin: 10,
-        flex: 1,
-      }}>
+    <View style={styles.container(height)}>
       <Image
-        source={require('../../../assets/images/dummy.jpg')}
-        style={{borderRadius: 3, width: '100%', height: '75%'}}
+        source={{uri: props.image}}
+        style={{borderRadius: 1, width: '99.5%', height: '75%'}}
       />
       <Text style={{fontFamily: 'Poppins-Medium', fontSize: 17}}>
-        Bunga Angrek
+        {props.flower}
       </Text>
       <Text style={{fontFamily: 'Poppins-Medium', fontSize: 15}}>
-        Rp. 150.000
+        {`Rp. ${props.price}`}
       </Text>
 
       <View
@@ -37,10 +28,10 @@ function Card() {
           justifyContent: 'center',
           alignItems: 'center',
         }}>
-        <TouchableOpacity onPress={handleClick}>
+        <TouchableOpacity onPress={() => handleClick(props.id)}>
           <Image
             source={require('../../../assets/icons/add.png')}
-            style={{width: 20, height: 20, margin: 4}}
+            style={{width: 22, height: 20, margin: 4}}
           />
         </TouchableOpacity>
       </View>
@@ -49,3 +40,16 @@ function Card() {
 }
 
 export default Card;
+
+const styles = StyleSheet.create({
+  container: height => ({
+    height: height,
+    alignItems: 'center',
+    borderRadius: 2,
+    borderColor: 'black',
+    borderWidth: 0.5,
+    backgroundColor: 'white',
+    margin: 10,
+    flex: 1,
+  }),
+});
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -12,12 +12,25 @@ function Home(props) {
   const {id} = props.route.params;
 
   const [user, setUser] = useState({});
+  const [flowers, setFlowers] = useState([]);
 
   useEffect(() => {
     var starCountRef = firebase.database().ref('users/' + id);
     starCountRef.on('value', snapshot => {
       const data = snapshot.val();
       setUser(data);
+    });
+
+    // get flowers
+    const starCountRef2 = firebase.database().ref('flowers/');
+    starCountRef2.on('value', snapshot => {
+      const data = snapshot.val();
+      const productArray = [];
+      Object.keys(data).map(i => {
+        return productArray.push({id: i, ...data[i]});
+      });
+      setFlowers(productArray);
+      // setFlowers(Object.entries(data).map(([key, value]) => ({key, value})));
     });
   }, []);
 
@@ -29,7 +42,7 @@ function Home(props) {
       <Tab.Screen
         name="Home"
         // component={HomeContent}
-        children={() => <HomeContent user={user} />}
+        children={() => <HomeContent user={user} flowers={flowers} />}
         options={{
           tabBarLabel: '',
           tabBarIcon: ({color}) => (
@@ -51,27 +64,74 @@ function Home(props) {
   );
 }
 
-function HomeContent(user) {
-  user = user.user;
+function HomeContent(props) {
+  const user = props.user;
+  const flowers = props.flowers;
+  console.log(flowers.length);
+  // const [flowers, setFlowers] = useState([]);
+
+  // useEffect(() => {
+  //   // get flowers
+  //   const starCountRef = firebase.database().ref('flowers/');
+  //   starCountRef.on('value', snapshot => {
+  //     const data = snapshot.val();
+  //     const productArray = [];
+  //     Object.keys(data).map(i => {
+  //       return productArray.push({id: i, ...data[i]});
+  //     });
+  //     setFlowers(productArray);
+  //     // setFlowers(Object.entries(data).map(([key, value]) => ({key, value})));
+  //   });
+  // }, []);
+  if (flowers.length === 0) {
+    return (
+      <View>
+        <Text>Loading</Text>
+      </View>
+    );
+  }
 
   return (
     <ScrollView style={{flex: 1}}>
       <HeaderUser image={user.imageBase64} />
-
-      <View style={styles.contentWrapper}>
-        <Card />
-        <Card />
-      </View>
-
-      <View style={styles.contentWrapper}>
-        <Card />
-        <Card />
-      </View>
-
-      <View style={styles.contentWrapper}>
-        <Card />
-        <Card />
-      </View>
+      {/* <Card image={i.image} flower={i.name} price={i.price} id={i.id} /> */}
+      {console.log('===========')}
+      {flowers.map((i, index, elements) => {
+        console.log(index % 2 === 0);
+        if (index % 2 === 0) {
+          if (elements[index + 1] !== undefined) {
+            return (
+              <View style={styles.contentWrapper}>
+                <Card
+                  image={i.image}
+                  flower={i.name}
+                  price={i.price}
+                  id={i.id}
+                />
+                <Card
+                  image={elements[index + 1].image}
+                  flower={elements[index + 1].name}
+                  price={elements[index + 1].price}
+                  id={elements[index + 1].id}
+                />
+              </View>
+            );
+          } else {
+            return (
+              <View style={styles.contentWrapper}>
+                <Card
+                  image={i.image}
+                  flower={i.name}
+                  price={i.price}
+                  id={i.id}
+                  height={500}
+                />
+              </View>
+            );
+          }
+        }
+      })}
+      {/* <Card /> */}
     </ScrollView>
   );
 }
