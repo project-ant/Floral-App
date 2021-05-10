cf081f7794e39ed42ed42d78efe895ce32e98cd5
project-ant <s21810297@student.unklab.ac.id>
2021-05-10 14:44:15 +0800

    Update Firebase

--- src/config/Firebase/index.js
+++ src/config/Firebase/index.js
@@ -1,13 +1,14 @@
 import firebase from 'firebase';
 
 var firebaseConfig = {
-  apiKey: 'AIzaSyDpmGGbZvkncCkutw3CxQfVdb4SLV20pkw',
-  authDomain: 'floral-app-f471a.firebaseapp.com',
-  projectId: 'floral-app-f471a',
-  storageBucket: 'floral-app-f471a.appspot.com',
-  messagingSenderId: '23457594764',
-  appId: '1:23457594764:web:dc41f10f1c681944c0eaa9',
-  measurementId: 'G-NWVXSRKRL6',
+  apiKey: "AIzaSyDpmGGbZvkncCkutw3CxQfVdb4SLV20pkw",
+  authDomain: "floral-app-f471a.firebaseapp.com",
+  databaseURL: "https://floral-app-f471a-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "floral-app-f471a",
+  storageBucket: "floral-app-f471a.appspot.com",
+  messagingSenderId: "23457594764",
+  appId: "1:23457594764:web:dc41f10f1c681944c0eaa9",
+  measurementId: "G-NWVXSRKRL6"
 };
 
 
