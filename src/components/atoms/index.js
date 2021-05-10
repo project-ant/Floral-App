56d149a31b713b36998fa1a9de341e18a0ee214a
KevinWullur <kevinwullur21@gmail.com>
2021-05-10 11:38:43 +0800

    submit icon

--- src/components/atoms/index.js
+++ src/components/atoms/index.js
@@ -1,6 +1,5 @@
 import Gap from './Gap'
 import TextInput from './TextInput'
 import Button from './Button'
-import Card from './Card'
 
-export {Gap, TextInput, Button, Card}
+export {Gap, TextInput, Button, }
