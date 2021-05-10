cf65455db01998e2b0f6f74c7d9ee98e9a2405f0
project-ant <s21810297@student.unklab.ac.id>
2021-05-10 11:54:27 +0800

    Update index.js remove unnecessary

--- src/components/atoms/index.js
+++ src/components/atoms/index.js
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
-import Gap from './Gap'
-import TextInput from './TextInput'
-import Button from './Button'
 
-export {Gap, TextInput, Button, }
-=======
 import Gap from './Gap';
 import TextInput from './TextInput';
 import Button from './Button';
 import Hr from './Hr';
 
 export {Gap, TextInput, Button, Hr};
->>>>>>> c9341f929907794fd561420e4dd3ab982a7a0985
