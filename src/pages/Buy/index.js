488d14f7a96c04d47417dc0b7a9b3e2e3b541cf9
project-ant <s21810297@student.unklab.ac.id>
2021-05-10 11:03:33 +0800

    Pages Dummy Buy Functional Button Nav

--- src/pages/Buy/index.js
+++ src/pages/Buy/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
 
-import {HeaderUser} from '../../components/molecules';
+import { Header} from '../../components/molecules';
+import {Gap, Button} from '../../components/atoms';
 
-function Buy() {
+const Tab = createMaterialBottomTabNavigator();
+
+function Buy(props) {
   return (
     <View>
-      <HeaderUser />
-
+      <Header
+        label="Floral-App"
+        bgColor="#FFC700"
+        miniText="Choose your flower"
+        onBack={() => props.navigation.goBack()}
+      />
       <Text>Dummy Buy Page</Text>
     </View>
   );
